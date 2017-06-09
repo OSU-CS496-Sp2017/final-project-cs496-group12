ae202b443aa0b4557268f278934393156f7f45b1
Luay Alshawi <loay.vb@gmail.com>
2017-06-08 19:44:17 -0700

    add menu item icon to show starred view

--- LowPolyFinalProject/app/src/main/java/com/cs496/clh/lowpolyfinalproject/MainActivity.java
+++ LowPolyFinalProject/app/src/main/java/com/cs496/clh/lowpolyfinalproject/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainactivitymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.starred_menu:
+                //Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                //startActivity(settingsIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     private void fetchImage(String url)
     {
         Intent intent = new Intent(this, SearchResultImageActivity.class);
